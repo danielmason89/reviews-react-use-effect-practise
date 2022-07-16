eab6300e77a12a0a59cbc1d69b595f7f0879f2eb
Daniel Mason <danmasson0@gmail.com>
2022-07-15 22:05:31 -0400

    implement continuity to app to avoid repetition

--- src/Review.js
+++ src/Review.js
@@ -29,6 +29,14 @@ const Review = () => {
       return checkNumber(newIndex);
     });
   };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
