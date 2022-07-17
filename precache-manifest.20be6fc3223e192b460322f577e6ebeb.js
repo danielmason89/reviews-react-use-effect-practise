132ec8e237944fd1ea9b629dc588deb60eb267ee
Daniel Mason <danmasson0@gmail.com>
2022-07-17 18:02:22 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.20be6fc3223e192b460322f577e6ebeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df4091f5744459e9fffb568771f263a",
+    "url": "/danielmason89/reviews-react-use-effect-practise.git/index.html"
+  },
+  {
+    "revision": "86fef3fea7a8bd496378",
+    "url": "/danielmason89/reviews-react-use-effect-practise.git/static/css/main.684b11a9.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/danielmason89/reviews-react-use-effect-practise.git/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/danielmason89/reviews-react-use-effect-practise.git/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fef3fea7a8bd496378",
+    "url": "/danielmason89/reviews-react-use-effect-practise.git/static/js/main.03b7e9a0.chunk.js"
+  },
+  {
+    "revision": "3b006cf84e8a826bad42",
+    "url": "/danielmason89/reviews-react-use-effect-practise.git/static/js/runtime-main.221fab4c.js"
+  }
+]);
